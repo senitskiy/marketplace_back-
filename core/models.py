d4e42bd1fe546dc7bba796edebb8e3dc191b2205
ruha02 <ruslan.gzn@gmail.com>
2022-05-26 17:43:35 +0500

    Update models

--- core/models.py
+++ core/models.py
@@ -1,38 +1,46 @@
+from pydoc import describe
 from django.db import models
 
 
 class User(models.Model):
     address = models.CharField(max_length=300,  unique=True, primary_key=True)
-    image = models.CharField(max_length=300, null=True, blank=True)
-    name = models.CharField(max_length=300, null=True, blank=True)
-    email = models.CharField(max_length=300, null=True, blank=True)
+    alias = models.CharField(max_length=300, unique=True) 
 
+    displayName = models.CharField(max_length=300, null=True, blank=True)
+    avatar = models.CharField(max_length=300, null=True, blank=True)
+    avatarMNFT = models.ForeignKey(to='MNFT', on_delete=models.CASCADE, related_name='avatars', null=True, blank=True)
+    #TODO: сделать логику заполнения alias если пусто то =address
+    #TODO: сделать логику выдачи аватара
+
+
+class Blockchain(models.Model):
+    name = models.CharField(max_length=30)
+    idChain = models.IntegerField(default=0)
+    rpcURL = models.URLField()
+    browserURL = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class MNFTCollection(models.Model):
+    alias = models.CharField(max_length=300, unique=True)
+    address = models.CharField(max_length=50)
+    displayName = models.CharField(max_length=300)
+    symbol = models.CharField(max_length=10)
+    description = models.TextField(null=True, blank=True)
+    alias = models.CharField(max_length=300, unique=True)
+    #TODO: сделать логику заполнения alias если пусто то =address
+
+    blockchain = models.ForeignKey(to="Blockchain", on_delete=models.CASCADE, related_name='mnft_collections')
 
 class MNFT(models.Model):
-    types_blockchain = (
-        (0, "ETHEREUM"),
-        (1, "SOLANA"),
-        (2, "TEZOS"),
-        (3, "EVERSCALE"),
-    )
-
-    types_standart = (
-        (721, "ERC721"),
-        (1155, "ERC1155"),
-    )
-    address = models.CharField(max_length=300, unique=True, primary_key=True,)
-    blockchain = models.IntegerField(default=0, choices=types_blockchain)
-    symbol = models.CharField(max_length=300, null=True, blank=True)
-    standart = models.IntegerField(default=721, choices=types_standart)
-    lastUpdate = models.DateField(auto_now=True)
-    name = models.CharField(max_length=300)
+    collection = models.ForeignKey(to='MNFTCollection', on_delete=models.CASCADE, related_name='mnfts')
+    tokenId = models.IntegerField(default=0)
+    creatorAddress = models.ForeignKey(to='User', on_delete=models.CASCADE, related_name="mnfts_creator")
+    ownerAddress = models.ForeignKey(to='User', on_delete=models.CASCADE, related_name="mnfts_owner")  
+    sponsorAddress = models.ForeignKey(to='User', on_delete=models.CASCADE, related_name="mnfts_lord", null=True, blank=True)
+    title = models.CharField(max_length=300)
     description = models.CharField(max_length=300, null=True, blank=True)
-    image = models.CharField(max_length=300)
-    cost = models.FloatField(default=0.0)
-    costAd = models.FloatField(default=0.0)
-    creator = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="mnfts_creator")
-    owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="mnfts_owner")
-    sponsor = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="mnfts_lord", null=True, blank=True)
+    price = models.FloatField(default=0.0)
+    priceAd = models.FloatField(default=0.0)
